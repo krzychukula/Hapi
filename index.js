c0c3918831edf792b7775d73fd0284415e9210fb
Krzysztof Kula <krzychukula@gmail.com>
2014-09-29 21:48:50 +0200

    add Good logging

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 var Hapi = require('hapi');
+var Good = require('good');
 
 // Create a server with a host and port
-var server = new Hapi.Server('localhost', 8000);
+var server = new Hapi.Server(8000);
 
 // Add the route
 server.route({
     method: 'GET',
-    path: '/hello',
+    path: '/',
     handler: function (request, reply) {
 
         reply('hello world');
     }
 });
 
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
 // Start the server
 server.start();
+
+server.pack.register(Good, function (err) {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start(function () {
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+});
