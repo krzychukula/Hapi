ff5fa1e8a99ec229972418edc3363b032bbb0e2d
Krzysztof Kula <krzychukula@gmail.com>
2014-11-12 13:51:56 +0100

    validation

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var Hapi = require('hapi');
 var path = require('path');
 var fs = require('fs');
 var rot13 = require('rot13-stream')();
+var Joi = require('joi');
 
 // Create a server with a host and port
 var server = Hapi.createServer('localhost', Number(process.argv[2] || 8080));
@@ -15,10 +16,26 @@ server.views({
   path: path.join(__dirname, 'templates')
 })
 
+
+var routeConfig = {
+  path: '/chickens/{breed}',
+  method: 'GET',
+  handler: function(req, rep){
+    reply('chicken' + req.params.breed)
+  },
+  config: {
+    validate: {
+      params: {
+        breed: Joi.string().required()
+      }
+    }
+  }
+}
+
 // Add the route
 server.route({
     method: 'GET',
-    path: '/',
+    path: '/rot',
     handler: function (request, reply){
       reply(fs.createReadStream('file.txt').pipe(rot13));
     }
