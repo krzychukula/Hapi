cb94a13dee44c16e873d61a91ad491eedca68f74
Krzysztof Kula <krzychukula@gmail.com>
2014-11-12 13:31:45 +0100

    Views

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var Hapi = require('hapi');
+var path = require('path')
 
 // Create a server with a host and port
 var server = Hapi.createServer('localhost', Number(process.argv[2] || 8080));
+
+server.views({
+  engines: {
+    html: require('handlebars')
+  },
+  path: path.join(__dirname, 'templates')
+})
 
 // Add the route
 server.route({
     method: 'GET',
     path: '/',
     handler: {
-      file: "public/index.html"
+      view: "index.html"
     }
 });
 
