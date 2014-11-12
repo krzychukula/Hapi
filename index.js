b2a3f8a497775c7c451f72b9dfe52fac5037bb31
Krzysztof Kula <krzychukula@gmail.com>
2014-11-12 11:25:00 +0100

    Update
    follow makemehappy tutorial

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 var Hapi = require('hapi');
-var Good = require('good');
-var Path = require('path');
 
-var serverOptions = {
-    views: {
-        engines: {
-            html: require('handlebars')
-        },
-        path: Path.join(__dirname, 'views')
-    }
-};
 // Create a server with a host and port
-var server = new Hapi.Server(8000, serverOptions);
+var server = Hapi.createServer('localhost', Number(process.argv[2] || 8080));
 
 // Add the route
 server.route({
@@ -19,37 +9,9 @@ server.route({
     path: '/',
     handler: function (request, reply) {
 
-        reply.view('index', { title: 'My home page' });
-    }
-});
-
-server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-    }
-});
-
-server.route({
-    method: 'GET',
-    path: '/public/{param*}',
-    handler: {
-        directory: {
-            path: 'public'
-        }
+        reply('Hello Hapi');
     }
 });
 
 // Start the server
 server.start();
-
-server.pack.register(Good, function (err) {
-    if (err) {
-        throw err; // something bad happened loading the plugin
-    }
-
-    server.start(function () {
-        server.log('info', 'Server running at: ' + server.info.uri);
-    });
-});
